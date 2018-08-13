f2714c034f91dc87859aa8e1dff324885f9c074d
Galih <Galih@LAPTOP-S8IDLHKL>
2018-08-13 13:50:57 +0700

    initial commit

---
+++ src/js11/ConvertVocal2Angka.java
@@ -0,0 +1,10 @@
+package js11;
+
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+    private static void tampilJudul (String identitas) {
+        System.out.println("IdentitaS : "+identitas);
+        
+       
+        }
+}
