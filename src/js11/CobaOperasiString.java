e629d597043ab24eeeaa51a17031e6a864fe3aa5
Galih <Galih@LAPTOP-S8IDLHKL>
2018-08-13 13:34:44 +0700

    Coba Method Length & isEmpty

--- src/js11/CobaOperasiString.java
+++ src/js11/CobaOperasiString.java
@@ -1,8 +1,21 @@
 package js11;
 
+import java.util.Locale;
+
 public class CobaOperasiString {
     public static void main(String[] args){
         String identitas = "Galih Satria Rachmanda/X RPL 2/16";
         System.out.println("Identitas: "+identitas);
+        
+        String x ="Operasi";
+        System.out.println("isi variabel x : "+x);
+        System.out.println("\""+x+"\"Panjang Karakter = "+x.length());
+        
+        System.out.println("x adalah kosong : "+x.isEmpty());
+        
+        String  y =""; 
+        System.out.println("Isi variabel y : "+y.length());
+        System.out.println("y sama dengan kosong : "+y.isEmpty());
+        
     }
 }
