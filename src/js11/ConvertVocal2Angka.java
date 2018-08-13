5a77d8867bbc4640612790595e3f40a08d603825
Galih <Galih@LAPTOP-S8IDLHKL>
2018-08-13 13:53:36 +0700

    Tambah Input

--- src/js11/ConvertVocal2Angka.java
+++ src/js11/ConvertVocal2Angka.java
@@ -5,6 +5,21 @@ public class ConvertVocal2Angka {
     private static void tampilJudul (String identitas) {
         System.out.println("IdentitaS : "+identitas);
         
-       
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+        public static void main(String[] args){
+        String identitas = "Galih Satria Rachmanda / X RPL 2 / 16";
+        
+        tampilJudul(identitas);
+        String kalimat = tampilInput();
         }
+        private static String tampilInput(){
+            Scanner scanner=new Scanner(System.in);
+            
+            System.out.println("Masukan kalimat: ");
+            String kalimat = scanner.nextLine();
+            System.out.println("Kalimat asli: " + kalimat);
+            
+            return kalimat;
+    }
 }
